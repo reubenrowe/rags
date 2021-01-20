1a8e8ec382cb650e37d9655212cc84a1cdb5de28
unironically <lbvac230@gmail.com>
2021-01-20 11:37:32 +0000

    Updated Rule class to allow a list of conditions over variables.

--- src/rnsr/rag/grammar/Rule.java
+++ src/rnsr/rag/grammar/Rule.java
@@ -1,9 +1,9 @@
 package rnsr.rag.grammar;
 
-import java.util.ArrayList;
-
 import rnsr.rag.grammar.exception.CloneException;
 import rnsr.rag.grammar.exception.VariableNotFoundException;
+
+import java.util.ArrayList;
 
 /**
  * A Rule production for a rescursive adaptive grammar (RAG)
@@ -13,6 +13,7 @@ public	class		Rule
 {
 
 	private ArrayList<Variable> m_arguments = null;
+	private ArrayList<VariableCondition> m_conditions = null;
 	
 	/**
 	 * Default Constructor
@@ -25,23 +26,39 @@ public	class		Rule
 	 * @param variables - the variable map used by this rule form
 	 * @param result - the sematic value of this rule form
 	 */
-	public Rule(Configuration configuration, VariableSet variables, Polynomial result)
+	public Rule(Configuration configuration, VariableSet variables, Polynomial result)  throws VariableNotFoundException
 	{
-		this.m_configuration = configuration;
-		this.m_variables = variables;
-		this.m_result = result;
+		this(configuration, variables, result, null, null);
 	}
-	
+
 	/**
 	 * Constructs a new Rule object
 	 * @param configuration - The configuration of terms represented by this rule form
 	 * @param variables - the variable map used by this rule form
 	 * @param result - the sematic value of this rule form
 	 * @param args - the argument list for this rule form
+	 * @throws VariableNotFoundException
 	 */
 	public Rule(Configuration configuration, VariableSet variables, Polynomial result, ArrayList<Variable> args) throws VariableNotFoundException
 	{
-		this(configuration, variables, result);
+		this(configuration, variables, result, args, null);
+	}
+
+	/**
+	 * Constructs a new Rule object
+	 * @param configuration - The configuration of terms represented by this rule form
+	 * @param variables - the variable map used by this rule form
+	 * @param result - the sematic value of this rule form
+	 * @param args - the argument list for this rule form
+	 * @param conditions - list of conditions on the variables contained in this rule
+	 */
+	public Rule(Configuration configuration, VariableSet variables, Polynomial result, ArrayList<Variable> args, ArrayList<VariableCondition> conditions) throws VariableNotFoundException
+	{
+		//this(configuration, variables, result);
+		this.m_configuration = configuration;
+		this.m_variables = variables;
+		this.m_result = result;
+		this.m_conditions = conditions;
 		this.setArguments(args);
 	}
 	
